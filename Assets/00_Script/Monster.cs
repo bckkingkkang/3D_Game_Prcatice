a2b8755d9d36d5b6dd51272ba3e0c7da7dec386f
가현 <bufar1014@gmail.com>
2024-11-19 04:00:49 +0900

    bullet, monster onTriggerEnter 적용

--- Assets/00_Script/Monster.cs
+++ Assets/00_Script/Monster.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
     private float speed = 2;
 
+    public int HP = 5;
+
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            HP--;
+            Destroy(other.transform.parent.gameObject);
+        }
     }
 }
