6fb1fe7a2bdc06354031d225683d3bd6b280c87b
가현 <bufar1014@gmail.com>
2024-11-24 06:48:22 +0900

    Item speed 설정, Monster와 마찬가지로 앞으로 이동하도록(가까워지도록) 코드 작성

--- Assets/00_Script/Item.cs
+++ Assets/00_Script/Item.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 2;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += -transform.forward * Time.deltaTime * speed;
     }
 }
