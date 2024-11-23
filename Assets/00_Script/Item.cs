516f6adb4bcb75ca987c0e1602993d9d31d79c54
가현 <bufar1014@gmail.com>
2024-11-24 07:33:11 +0900

    state 배열에 랜덤으로 ATK_Speed, ATK_Count 넣기
    ATK_Speed, ATK_Count를 처음부터 text, material을 지정해두지 않고
    ATK_Speed, ATK_Count를 Randorm으로 받아와서 해당하는 text와 material을 적용한다.
    
    현재 랜덤으로 state 배열에 저장하는 과정까지 작성

--- Assets/00_Script/Item.cs
+++ Assets/00_Script/Item.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Item_State
+{
+    ATK_Speed,
+    ATK_Count
+}
+
 public class Item : MonoBehaviour
 {
     private float speed = 2;
 
     public GameObject[] Cubes;
 
-    public enum Item_State
-    {
-        ATK_Speed,
-        ATK_Count
-    }
-    public Item_State state;
+    Item_State[] state = new Item_State[2];
 
     private void Start()
     {
-        
+        state[0] = (Item_State)Random.Range(0, 2);
+
+        /*
+        if(Item_State.ATK_Speed == state[0])
+        {
+            state[1] = Item_State.ATK_Count;
+        } else
+        {
+            state[1] = Item_State.ATK_Speed;
+        }
+        */
+        state[1] = Item_State.ATK_Speed == state[0] ? Item_State.ATK_Count : Item_State.ATK_Speed;
     }
 
     private void Update()
